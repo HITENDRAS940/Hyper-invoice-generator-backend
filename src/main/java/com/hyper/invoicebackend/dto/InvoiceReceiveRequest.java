bec7dfc38261934fa19a18bbbb4a0794d1592911
hitendrasingh <hitendras940@gmail.com>
2026-02-21 23:06:36 +0530

    Bug

--- src/main/java/com/hyper/invoicebackend/dto/InvoiceReceiveRequest.java
+++ src/main/java/com/hyper/invoicebackend/dto/InvoiceReceiveRequest.java
@@ -1,5 +1,6 @@
 package com.hyper.invoicebackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InvoiceReceiveRequest {
     private Long bookingId;
+
+    @JsonProperty("invoiceURL")
     private String invoiceUrl;
 }
 
